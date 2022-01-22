4e344137d720131f66a8556ab8d7ac33a13b4305
radhika <rvramayra1998@gmail.com>
2022-01-22 11:21:35 +0530

    all files

---
+++ land_js.js
@@ -0,0 +1,36 @@
+
+  // document.getElementById("new").addEventListener("click", function () {
+  //   window.location.href = "#";
+  // });
+  document.getElementById("nav").addEventListener("click", function () {
+    window.location.href = "land_page.html";
+  });
+  document.getElementById("men").addEventListener("click", function () {
+    window.location.href = "man.html";
+  });
+  document.getElementById("women").addEventListener("click", function () {
+    window.location.href = "women.html";
+  });
+  document.getElementById("kids").addEventListener("click", function () {
+    window.location.href = "kids.html";
+  });
+  
+  document.getElementById("signin").addEventListener("click", function () {
+    window.location.href = "signin.html";
+  });
+  // document.getElementById("cas").addEventListener("click", function () {
+  //   window.location.href = "cas.html";
+  // });
+  // document.getElementById("sale").addEventListener("click", function () {
+  //   window.location.href = "sale.html";
+  // });
+  // document.getElementById("search").addEventListener("click", function () {
+  //   window.location.href = "search.html";
+  // });
+  // 
+  // document.getElementById("login").addEventListener("click", function () {
+  //   window.location.href = "login.html";
+  // });
+  // document.getElementById("help").addEventListener("click", function () {
+  //   window.location.href = "help.html";
+  // });
